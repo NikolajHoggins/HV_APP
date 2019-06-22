c77961dcfc9cf24ccabbe7cf5c9b87589f87f3aa
Nikolaj Hoggins Jacobsen <mollevip@gmail.com>
2019-06-22 21:57:12 +0100

    Post skabelon update
    url/cardsetup

--- resources/views/testviews/cardsetup.blade.php
+++ resources/views/testviews/cardsetup.blade.php
@@ -6,21 +6,47 @@
 
 	</head>
 	<body>
-	<div class="container">
+	<div class="container text-center">
 		<div class="card-deck mb-3 text-center mt-3">
 		  <div class="card mb-4 shadow-sm">
 		    <div class="card-header">
-		      <h4 class="my-0 font-weight-normal text-left">Sælger: Jacobsen VVS, Sælger kloakmester</h4>
+		      <h4 class="my-0 font-weight-bold text-left">Sælger: Jacobsen VVS, Sælger kloakmester</h4>
 		    </div>
 		    <div class="card-body">
-		      <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-		      <ul class="list-unstyled mt-3 mb-4">
-		        <li>10 users included</li>
-		        <li>2 GB of storage</li>
-		        <li>Email support</li>
-		        <li>Help center access</li>
-		      </ul>
-		      <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+		    	<div class="container">
+		    		<div class="row">
+		    			<div class="col col-lg-6">
+		    				<h1 class="card-title pricing-card-title">Information om ansat</h1>
+		    			</div>
+		    			<div class="col col-lg-6">
+		    				<h1 class="card-title pricing-card-title">Yderligere information</h1>
+
+		    			</div>
+		    		</div>
+		    		<div class="row">
+						<div class="col col-lg-2">
+							<p class="font-weight-bold text-left">Firma:</p>
+							<p class="font-weight-bold text-left">Position:</p>
+							<p class="font-weight-bold text-left">Navn:</p>
+							<p class="font-weight-bold text-left">Alder:</p>
+							<p class="font-weight-bold text-left">Erfaring:</p>
+							<p class="font-weight-bold text-left">Tlf:</p>
+						</div>
+						<div class="col col-lg-4">
+							<p class="text-left">Jacobsen VVS</p>
+							<p class="text-left">Kloakmester</p>
+							<p class="text-left">Jonas Larsen</p>
+							<p class="text-left">34 år</p>
+							<p class="text-left">14 år</p>
+							<p class="text-left">+45 88 88 88 88</p>
+						</div>
+		    			<div class="col col-lg-6">
+		    				<p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		    				
+		    			</div>
+		    		</div>
+		    	</div>
+		      <button type="button" class="btn btn-lg btn-block btn-outline-primary mt-4">Læs mere</button>
 		    </div>
 		  </div>
 	</div>
